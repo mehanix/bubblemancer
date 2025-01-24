af16ea32b49f2d97d11797b804756c0493438781
Nicoleta Ciausu <nicoleta.ciausu@outlook.com>
2025-01-25 00:39:54 +0200

    no bubble overlap

--- bubblejam/Scripts/BubbleSpawner.gd
+++ bubblejam/Scripts/BubbleSpawner.gd
@@ -13,12 +13,26 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("click"):
-		var bubble = bubblePart.instantiate()
+		var bubbles_on_screen = get_tree().get_nodes_in_group("bubbles")
 		var mouse = get_global_mouse_position()
 		var position_x = mouse.x - (int(mouse.x) % 5)
 		var position_y = mouse.y - (int(mouse.y) % 5)
+		var obj = get_object_at_mouse_pos(position_x, position_y)
+		print(obj)
+		if (len(obj) > 0): return
+		var newBubble = bubblePart.instantiate()
+		newBubble.add_to_group("instantiated_bubbles")
 
-		bubble.position = Vector2(position_x, position_y)
-		add_child(bubble) 
+
+		newBubble.position = Vector2(position_x, position_y)
+		add_child(newBubble) 
 		
 	pass
+
+func get_object_at_mouse_pos(position_x, position_y):
+	var space_state = get_world_2d().direct_space_state
+	# use global coordinates, not local to node
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = Vector2(position_x, position_y)
+	var result = space_state.intersect_point(query)
+	return result
