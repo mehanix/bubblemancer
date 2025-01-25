b3e083050bb4aba8e46bed824b0f1d00f12b066c
foxarios <theromanianfox@gmail.com>
2025-01-25 13:59:01 +0200

    Movement 2.0

--- bubblejam/Scripts/player.gd
+++ bubblejam/Scripts/player.gd
@@ -1,47 +1,52 @@
 extends CharacterBody2D
 
-
-const SPEED = 150.0
-const JUMP_VELOCITY = -200.0
+@export_category("Movement")
+@export var SPEED : int = 150.0
+@export var JUMP_FORCE : int = 200.0
+@export var Gravity : int = 900
+var Jump_Available: bool = false
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		animated_sprite_2d.play("JumpFall")
-		
-		
 
-	
-		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("MoveLeft", "MoveRight")
-		
-	#Flip Sprite
-	if direction > 0:
-		animated_sprite_2d.flip_h = false	
-	elif direction <0: 
-			animated_sprite_2d.flip_h = true
-	
-	if is_on_floor():
-		if direction == 0: 
-			animated_sprite_2d.play("Idle")
-		else:
-			animated_sprite_2d.play("Run")
 
-			
-	#Movment
-	if direction:
+func _physics_process(delta):
+	var direction = Input.get_axis("MoveLeft", "MoveRight")
+	
+	#Movement
+	
+	if direction: 
 		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			animated_sprite_2d.play("Run")
 		
-		# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		animated_sprite_2d.play("JumpStart")
+		#Flip Sprite
+		if direction == 1:
+			animated_sprite_2d.flip_h = false
+		elif direction == -1:
+			animated_sprite_2d.flip_h = true
+		
+		
+	else:
+		velocity.x = 0
+		if is_on_floor():
+			animated_sprite_2d.play("Idle")
+	
+	#Gravity
+	
+	if not is_on_floor():
+		velocity.y += Gravity*delta
+		if velocity.y > 0 :
+			animated_sprite_2d.play ("JumpFall")
+	else: 
+		Jump_Available = true
 
+	#Jump
+	
+	if Input.is_action_just_pressed("Jump") and Jump_Available:
+		velocity.y -= JUMP_FORCE
+		Jump_Available = false
+		animated_sprite_2d.play("JumpStart")
+		
 	move_and_slide()
+	
