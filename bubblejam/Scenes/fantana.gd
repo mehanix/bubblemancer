7e1cbc247e9a0e6ab39037a42b6bc256f92ed852
Nicoleta Ciausu <nicoleta.ciausu@outlook.com>
2025-01-26 17:15:26 +0200

    win lose

--- bubblejam/Scenes/fantana.gd
+++ bubblejam/Scenes/fantana.gd
@@ -13,5 +13,5 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://Scenes/LevelDesign/Death/Death.tscn")
+		get_tree().change_scene_to_file("res://Scenes/LevelDesign/Death/Win.tscn")
 	pass # Replace with function body.
