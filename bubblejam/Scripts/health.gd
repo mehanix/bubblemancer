afea2c94647fcd2f469d9494bd7deddaaa6d8abd
Nicoleta Ciausu <nicoleta.ciausu@outlook.com>
2025-01-26 14:40:22 +0200

    nice health

--- bubblejam/Scripts/health.gd
+++ bubblejam/Scripts/health.gd
@@ -1,16 +1,13 @@
 extends Node
 
 @onready var game_manger: Node = %GameManger
-@onready var label: Label = $"../Camera2D/Label"
 
 var startHP : float
 
 func  _ready():
 	startHP = Global.playerHealth
 	
-
 func _process(delta):
-	
 	
 	if Global.playerHealth <= 0:
 		get_tree().change_scene_to_file("res://Scenes/LevelDesign/Menu/MainMenu.tscn")
